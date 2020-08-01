9458df9585fa9f63fbbf0280b854cb2d4c0064a8
Khrystyna Boychuk <khrystyna@Khrystynas-MacBook-Pro.local>
2020-07-31 19:33:07 -0700

    Flask serves React app

---
+++ teamfinder-server/static/react/precache-manifest.a6b9d7762483e8b6f8e5ef323e07c11c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b5d671fd3e5a04f3eb75d790a928aa",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "d8d28985f8b456df7a21",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4289806e36072b839182",
+    "url": "/static/react/js/2.64a91cbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.64a91cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d28985f8b456df7a21",
+    "url": "/static/react/js/main.1d48008d.chunk.js"
+  },
+  {
+    "revision": "a916e81baef4f8f5660d",
+    "url": "/static/react/js/runtime-main.775d3cf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
